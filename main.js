66ceb558c7cffe9bb3d8dbbe74243e9a5f15e07c
franwino <franciscowinocur@gmail.com>
2020-09-08 14:31:39 -0300

    navbar lista y comienzo hero

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const burger = document.getElementById("btn-burger");
-burger.addEventListener("click", e => {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("hidden");
-})
+burger.addEventListener("click", (e) => {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("open");
+  if (!menu.classList.contains("open")) {
+    burger.setAttribute("src", "assets/burger.svg");
+  }
+  else {
+    burger.setAttribute("src", "assets/close.svg");
+  }
+});
