db404e0e7bb50f12c1fa3c015c0abd837655adf6
franwino <franciscowinocur@gmail.com>
2020-09-09 00:01:33 -0300

    conexión API trending gifs y minor fixs

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Funcionamiento menu hamburguesa */
 const burger = document.getElementById("btn-burger");
 burger.addEventListener("click", (e) => {
   const menu = document.getElementById("menu");
@@ -9,3 +10,27 @@ burger.addEventListener("click", (e) => {
     burger.setAttribute("src", "assets/close.svg");
   }
 });
+
+
+/* Request trending GIFs */
+const url =
+  "http://api.giphy.com/v1/gifs/trending?api_key=KppHsCiWvApmVnuPSw2XE7mi3z6XR7s9&limit=10";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => createElement(data))
+  .catch((error) => console.log("error:", error));
+
+function createElement(data) {
+  data.data.forEach((gif) => {
+    const el = document.createElement("img");
+    el.src = gif.images.downsized.url;
+    el.style.width = "243px";
+    el.style.height = "187px";
+    el.alt = gif.images.title;
+    const cont = document.createElement("div");
+    cont.className = "gif-container"
+    cont.appendChild(el);
+    document.getElementById("trending_gifs").appendChild(cont);
+  });
+}
