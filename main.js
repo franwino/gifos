4a9d3dc0031756d9473470837d9db5f2de347975
franwino <franciscowinocur@gmail.com>
2020-10-01 01:30:37 -0300

    ver mas search - fav section - otros fixes

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const apikey = "?api_key=KppHsCiWvApmVnuPSw2XE7mi3z6XR7s9";
+/* Ejecuta las funciones necesarias al cargar el sitio */
+stickySearch();
+modoNocturno();
+autocomplete();
+trendingSearchs();
+trendingGifs();
 
 /* Mostrar/Ocultar elementos del DOM por id */
 function hide(id) {
@@ -8,6 +14,22 @@ function hide(id) {
 function unhide(id) {
   const el = document.getElementById(id);
   el.classList.remove("hidden");
+}
+
+/* Quitar clase active de todos los elementos */
+function quitarActive() {
+  const active = document.querySelectorAll(".active");
+  for (elem of active) {
+    if (elem.classList.contains("active")) {
+      elem.classList.remove("active");
+      break;
+    }
+  }
+}
+/* Hacer active solo un elemento */
+function makeActive(elem) {
+  quitarActive();
+  elem.classList.add("active");
 }
 
 /* Obtener coordenadas de un elemento */
@@ -36,24 +58,6 @@ function getPos(el) {
     y: yPos,
   };
 }
-
-/* Quitar clase active de todos los elementos */
-function quitarActive() {
-  const active = document.querySelectorAll(".active");
-  for (elem of active) {
-    if (elem.classList.contains("active")) {
-      elem.classList.remove("active");
-      break;
-    }
-  }
-}
-
-/* Hacer active solo un elemento */
-function makeActive(elem) {
-  quitarActive();
-  elem.classList.add("active");
-}
-
 /* Menu scroll - Sombre y searchbar sticky */
 function stickySearch() {
   const searchbar = document.getElementById("barra-busqueda");
@@ -76,7 +80,6 @@ function stickySearch() {
     }
   };
 }
-stickySearch();
 
 /* Funcionamiento menu hamburguesa */
 function menuHamburguesa() {
@@ -131,82 +134,231 @@ function modoNocturno() {
     localStorage.setItem("dark", darkMode);
   });
 }
-modoNocturno();
 
 /* Botón Home */
-function home() {
-  const btn = document.getElementById("btnHome");
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    unhide("inicio");
-    hide("seccion-busqueda");
-    hide("sec-misGifos");
-    hide("sec-favs");
-    hide("sec-crear");
-    quitarActive();
-    if (menu.classList.contains("open")) {
-      menuHamburguesa();
-    }
-    window.scrollTo(0, 0);
-  });
-}
-home();
-
-/* Favoritos */
-function favoritos() {
-  const btn = document.getElementById("btnFav");
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    hide("inicio");
-    hide("seccion-busqueda");
-    hide("sec-misGifos");
-    hide("sec-crear");
-    unhide("sec-favs");
+function menuHome() {
+  unhide("inicio");
+  hide("seccion-busqueda");
+  hide("sec-misGifos");
+  hide("sec-favs");
+  hide("sec-crear");
+  quitarActive();
+  if (menu.classList.contains("open")) {
     menuHamburguesa();
-    makeActive(btn);
-    window.scrollTo(0, 0);
+  }
+  window.scrollTo(0, 0);
+}
+
+let favs = [];
+favs = traerFavoritos();
+/* Buscar favs en localStorage */
+function traerFavoritos() {
+  if (localStorage.getItem("favs") != null) {
+    favs = JSON.parse(localStorage.getItem("favs"));
+  }
+  return favs;
+}
+
+/* Mostrar favoritos */
+function menuFavoritos(elem) {
+  hide("inicio");
+  hide("seccion-busqueda");
+  hide("sec-misGifos");
+  hide("sec-crear");
+  unhide("sec-favs");
+  menuHamburguesa();
+  makeActive(elem);
+  renderFavs();
+  window.scrollTo(0, 0);
+}
+let favsShown = 0;
+function renderFavs() {
+  favsShown = 0;
+  const grillaFav = document.getElementById("rdo-favs");
+  grillaFav.innerHTML = "";
+  if (favs.length !== 0) {
+    hide("fav-vacia");
+    unhide("rdo-favs");
+    masFavs();
+  } else {
+    hide("rdo-favs");
+    unhide("fav-vacia");
+  }
+}
+
+function masFavs() {
+  const grillaFav = document.getElementById("rdo-favs");
+  let n = Math.min(favs.length - favsShown, 12);
+  for (let i = 0; i < n; i++) {
+    grillaFav.innerHTML += renderGif(favs[favsShown]);
+    favsShown++;
+  }
+  const btn = document.getElementById("ver-mas-fav");
+  if (favsShown >= favs.length) {
+    btn.classList.add("hidden");
+  } else {
+    btn.classList.remove("hidden");
+  }
+}
+
+/* Favoritear */
+function fav(id, downsized, hires, user, title) {
+  const datos = {
+    id: id,
+    images: {
+      downsized: {
+        url: downsized,
+      },
+      downsized_large: {
+        url: hires,
+      },
+    },
+    username: user,
+    title: title,
+  };
+  favs.push(datos);
+  localStorage.setItem("favs", JSON.stringify(favs));
+  if (document.getElementById("btnFav").classList.contains("active")) {
+    renderFavs();
+  }
+}
+
+/* Menu - Mis Gifos */
+function menuMisGifos(elem) {
+  hide("inicio");
+  hide("seccion-busqueda");
+  hide("sec-favs");
+  hide("sec-crear");
+  unhide("sec-misGifos");
+  menuHamburguesa();
+  makeActive(elem);
+  window.scrollTo(0, 0);
+}
+
+/* Menu - Crear nuevos Gifos */
+function menuCrear(elem) {
+  hide("inicio");
+  hide("seccion-busqueda");
+  hide("sec-favs");
+  hide("sec-misGifos");
+  unhide("sec-crear");
+  menuHamburguesa();
+  makeActive(elem);
+  window.scrollTo(0, 0);
+}
+
+/* Descragar Gif */
+function download(id) {
+  console.log(id);
+}
+
+/* Maximizar GIF */
+function toggleMax(id) {
+  const elem = document.getElementById(id);
+  elem.classList.remove("min");
+  elem.classList.add("max");
+  /* Muestro y habilito boton de cerrar */
+  const close = elem.firstElementChild;
+  close.classList.remove("hidden");
+  /* Al maximizar cambio la url por una de mejor calidad */
+  const img = close.nextElementSibling;
+  const newurl = elem.getAttribute("data-hires");
+  img.src = newurl;
+  close.addEventListener("click", (e) => {
+    elem.classList.add("min");
+    elem.classList.remove("max");
+    close.classList.add("hidden");
   });
 }
-favoritos();
 
-/* Mis Gifos */
-function misGifos() {
-  const btn = document.getElementById("btnMis");
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    hide("inicio");
-    hide("seccion-busqueda");
-    hide("sec-favs");
-    hide("sec-crear");
-    unhide("sec-misGifos");
-    menuHamburguesa();
-    makeActive(btn);
-    window.scrollTo(0, 0);
-  });
+/* Renderizar un gif en un contenedor. Devuelve el HTML */
+function renderGif(gif) {
+  let user = gif.username;
+  if (user === "") {
+    user = "anonymous";
+  }
+  let titulo = gif.title;
+  if (titulo === "") {
+    titulo = "Sin título";
+  }
+  let card = "";
+  card += `<div class="gif-container min" data-hires="${gif.images.downsized_large.url}" id="${gif.id}">
+      <div class="close-max hidden"></div>
+      <img class="gif" src="${gif.images.downsized.url}" onclick='toggleMax("${gif.id}")' alt="${titulo}"/>
+      <div class="card-hover">
+        <div class="datos">
+          <p class="user">${user}</p>
+          <p class="gif-title">${titulo}</p>
+        </div>
+        <div class="card-btns">
+          <img onclick='fav("${gif.id}","${gif.images.downsized.url}}","${gif.images.downsized_large.url}}","${user}","${titulo}")' class="btn-fav" src="assets/icon-fav-hover.svg" alt="Agregar a favoritos"/>
+          <img onclick='download("${gif.id}")' class="btn-download" src="assets/icon-download-hover.svg" alt="Descargar GIF"/>
+          <img onclick='toggleMax("${gif.id}")' class="btn-max" src="assets/icon-max-hover.svg" alt="Maximizar"/>
+        </div>
+      </div>
+    </div>`;
+  return card;
 }
-misGifos();
 
-/* Crear nuevos Gifos */
-function crearGifos() {
-  const btn = document.getElementById("btnCrear");
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    hide("inicio");
-    hide("seccion-busqueda");
-    hide("sec-favs");
-    hide("sec-misGifos");
-    unhide("sec-crear");
-    menuHamburguesa();
-    makeActive(btn);
-    window.scrollTo(0, 0);
-  });
+/* Renderizar una grilla de gifs a partir de una busqueda */
+function renderGrilla(data) {
+  let grilla = "";
+  for (gif of data) {
+    grilla += renderGif(gif);
+  }
+  return grilla;
 }
-crearGifos();
 
-/* Auto Completar */
+/* Hacer la búsqueda y llamar a renderizarla */
+let searchOffset = 0;
+function doSearch(query) {
+  const limit = 12;
+  let offset = searchOffset;
+  const url = `https://api.giphy.com/v1/gifs/search${apikey}&q=${query}&limit=${limit}&offset=${offset}&rating=pg-13&lang=es`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      unhide("seccion-busqueda");
+      const header = document.getElementById("title-busq");
+      header.textContent = query;
+      const container = document.getElementById("rdo-busqueda");
+      if (data.pagination.total_count !== 0) {
+        const grilla = renderGrilla(data.data);
+        hide("busqueda-vacia");
+        unhide("rdo-busqueda");
+        if (offset == 0) {
+          container.innerHTML = grilla;
+        } else {
+          container.innerHTML += grilla;
+        }
+        const btn = document.getElementById("ver-mas-busqueda");
+        searchOffset += limit;
+        if (searchOffset > data.pagination.total_count) {
+          btn.classList.add("hidden");
+        } else {
+          btn.classList.remove("hidden");
+        }
+      } else {
+        hide("rdo-busqueda");
+        unhide("busqueda-vacia");
+      }
+    })
+    .catch((error) => console.log("error:", error));
+}
+
+function masSearch() {
+  const query = document.getElementById("title-busq").textContent;
+  doSearch(query);
+}
+
+/* Auto Completar y Buscar */
 function autocomplete() {
-  const lista = document.getElementById("autocomplete");
+  const input = document.getElementById("search");
   const btn = document.getElementById("buscarborrar");
+  const lista = document.getElementById("autocomplete");
+  function limpiarGrilla() {
+    lista.innerHTML = "";
+  }
   function request(query) {
     const url =
       "https://api.giphy.com/v1/gifs/search/tags" + apikey + "&q=" + query;
@@ -226,9 +378,10 @@ function autocomplete() {
           container.appendChild(el);
           el.addEventListener("click", (e) => {
             input.value = data.data[i].name;
-            doSearch();
+            searchOffset = 0;
+            doSearch(input.value);
             input.value = "";
-            request(input.value);
+            limpiarGrilla();
           });
           lista.appendChild(container);
         }
@@ -251,10 +404,10 @@ function autocomplete() {
       })
       .catch((error) => console.log("error:", error));
   }
-  const input = document.getElementById("search");
+
   btn.addEventListener("click", (e) => {
     input.value = "";
-    request(input.value);
+    limpiarGrilla();
   });
   input.addEventListener("input", (e) => {
     request(input.value);
@@ -262,103 +415,17 @@ function autocomplete() {
   input.addEventListener("keydown", (e) => {
     if (input.value !== "" && e.key == "Enter") {
       e.preventDefault();
-      doSearch();
+      searchOffset = 0;
+      doSearch(input.value);
       input.value = "";
-      request(input.value);
-    }
-    if (input.value !== "" && e.key == "ArrowDown") {
+      limpiarGrilla();
     }
   });
 }
-autocomplete();
 
-function fav(id) {
-  console.log(id);
-}
-
-function download(id) {
-  console.log(id);
-}
-
-/* Maximizar GIF */
-function toggleMax(id) {
-  const elem = document.getElementById(id);
-  elem.classList.remove("min");
-  elem.classList.add("max");
-  const close = elem.firstElementChild;
-  /* Muestro y habilito boton de cerrar */
-  close.classList.remove("hidden");
-  close.addEventListener("click", (e) => {
-    elem.classList.add("min");
-    elem.classList.remove("max");
-    close.classList.add("hidden");
-  });
-}
-
-/* Renderizar un gif en un contenedor. Devuelve el HTML */
-function renderGif(gif) {
-  let user = gif.username;
-  if (user === "") {
-    user = "anonymous";
-  }
-  let titulo = gif.title;
-  if (titulo === "") {
-    titulo = "Sin título";
-  }
-  let card = "";
-  card += `<div class="gif-container min" id="${gif.id}">
-      <i class="close-max fas fa-times hidden"></i>
-      <img class="gif" src="${gif.images.downsized.url}" onclick=toggleMax("${gif.id}") alt="${titulo}"/>
-      <div class="card-hover">
-        <div class="datos">
-          <p class="user">${user}</p>
-          <p class="gif-title">${titulo}</p>
-        </div>
-        <div class="card-btns">
-          <img onclick=fav("${gif.id}") class="btn-fav" src="assets/icon-fav-hover.svg" alt="Agregar a favoritos"/>
-          <img onclick=download("${gif.id}") class="btn-download" src="assets/icon-download-hover.svg" alt="Descargar GIF"/>
-          <img onclick=toggleMax("${gif.id}") class="btn-max" src="assets/icon-max-hover.svg" alt="Maximizar"/>
-        </div>
-      </div>
-    </div>`;
-  return card;
-}
-
-/* Renderizar una grilla de gifs a partir de una busqueda */
-function renderGrilla(data) {
-  let grilla = "";
-  data.data.forEach((gif) => {
-    grilla += renderGif(gif);
-  });
-  return grilla;
-}
-
-/* Hacer la búsqueda y llamar a renderizarla */
-function doSearch() {
-  let offset = 0;
-  const query = document.getElementById("search").value;
-  const limit = 12;
-  const url = `https://api.giphy.com/v1/gifs/search${apikey}&q=${query}&limit=${limit}&offset=${offset}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      unhide("seccion-busqueda");
-      const header = document.getElementById("title-busq");
-      header.textContent = query;
-      const container = document.getElementById("rdo-busqueda");
-      if (data.pagination.total_count !== 0) {
-        const grilla = renderGrilla(data);
-        container.innerHTML = grilla;
-        hide("busqueda-vacia");
-        unhide("rdo-busqueda");
-      } else {
-        hide("rdo-busqueda");
-        unhide("busqueda-vacia");
-      }
-      const gotoSearch = document.getElementById("goto-search");
-      gotoSearch.scrollIntoView();
-    })
-    .catch((error) => console.log("error:", error));
+function trendSearch(query) {
+  searchOffset = 0;
+  doSearch(query);
 }
 
 /* Trending Searchs */
@@ -374,15 +441,14 @@ function trendingSearchs() {
     const n = 5;
     rdo = "";
     for (let i = 0; i < n; i++) {
-      rdo += data.data[i];
+      rdo += `<span class="trend" onclick='trendSearch("${data.data[i]}")'> ${data.data[i]}</span>`;
       if (i < n - 1) {
         rdo += ", ";
       }
     }
-    document.getElementById("text-trends").innerText = rdo;
+    document.getElementById("text-trends").innerHTML = rdo;
   }
 }
-trendingSearchs();
 
 /* Trending GIFs */
 function trendingGifs() {
@@ -393,10 +459,9 @@ function trendingGifs() {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const grilla = renderGrilla(data);
+      const grilla = renderGrilla(data.data);
       const container = document.getElementById("trending-gifs");
       container.innerHTML = grilla;
     })
     .catch((error) => console.log("error:", error));
 }
-trendingGifs();
